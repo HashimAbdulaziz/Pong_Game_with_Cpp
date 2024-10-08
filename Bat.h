298b26cc5c1fa9e6f905bec4d5aafad6f1d63447
HashimAbdulaziz <hashimzezo222@gmail.com>
2024-10-09 02:19:12 +0300

    I added a Bat class

---
+++ Bat.h
@@ -0,0 +1,29 @@
+//
+// Created by HashimAbdulAziz on 10/9/2024.
+//
+
+#pragma once
+#include <SFML/Graphics.hpp>
+
+using namespace sf;
+
+class Bat {
+    private:
+        Vector2f m_Position;
+        RectangleShape m_Shape; // A RectangleShape object
+        float m_Speed = 1000.0f
+        bool m_MovingLeft = false;
+        bool m_MovingRight = false;
+
+    public:
+        Bat (float startX, float startY);
+
+        FloatRect getPosition();
+        RectangleShape getShape();
+        void moveLeft();
+        void moveRight();
+        void stopLeft();
+        void stopRight();
+        void update(Time dt);
+
+};
